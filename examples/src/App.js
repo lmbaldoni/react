9ef9da8a0f2b13833199b5e59471acd5b13f0879
DevIdCarvajal <davidcarvajalg@gmail.com>
2023-05-11 18:55:34 +0200

    useEffect examples

--- examples/src/App.js
+++ examples/src/App.js
@@ -1,13 +1,12 @@
 // Imports
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css"
 import dbImg from "./assets/images/dragon-ball.jpg"
 
 import CharactersList from "./components/CharacterList"
 
-// Function
-const App = () => {
-  const [characters, setCharacters] = useState([
+/*
+[
     {
       name: "Goku",
       race: "supersaiyan"
@@ -28,7 +27,12 @@ const App = () => {
       name: "Ginyu",
       race: ""
     }
-  ])
+  ]
+*/
+
+// Function
+const App = () => {
+  const [characters, setCharacters] = useState([])
   let [maximum, setMaximum] = useState(0)
   
   const increaseMaximum = () => setMaximum(maximum+1)
@@ -36,13 +40,32 @@ const App = () => {
   // Initialize data
   const titleApp = "Feliz Día de Goku"
 
+  // Side effect
+  useEffect(() => {
+
+    // Retrieve data
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(data => {
+        const dataFormatted = data.map(person => {
+          return {
+            name: person.name,
+            race: "supersaiyan"
+          }
+        })
+
+        setCharacters(dataFormatted)
+      })
+
+  }, []) // <---------- First render
+  
   return (
     <div>
-      <h1>{titleApp}</h1>
+      {/* <h1>{titleApp}</h1>
       <img className="poster" src={dbImg} alt="Imagen de Goku" />
 
       <p>{maximum}</p>
-      <button onClick={increaseMaximum}>Incrementar</button>
+      <button onClick={increaseMaximum}>Incrementar</button> */}
 
       <CharactersList dataChars={{characters, setCharacters}} maximum={maximum}/>
     </div>
--- examples/src/components/CharacterList.jsx
+++ examples/src/components/CharacterList.jsx
@@ -1,19 +1,35 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Character from "./Character"
 
 const CharactersList = ({dataChars, maximum}) => {
 
   const [name, setName] = useState("")
   const [race, setRace] = useState("")
+  const [search, setSearch] = useState("")
+  const [totalSaiyans, setTotalSaiyans] = useState(0)
 
   const addCharacter = () =>
     dataChars.setCharacters([...dataChars.characters, { name, race }])
 
   const handlerName = event => setName(event.target.value)
   const handlerRace = event => setRace(event.target.value)
+  const handlerSearch = event => setSearch(event.target.value)
 
   const seePosition = index => alert(dataChars.characters[index].name)
-
+  
+  useEffect(() => {
+    const ts = dataChars
+                  .characters
+                  .reduce((total, character) => {
+                    if(character.race === "supersaiyan")
+                      return total + 1
+                    else
+                      return total
+                  }, 0)
+                  
+    setTotalSaiyans(ts)
+  }, [dataChars])
+  
   return (
     <div>
       <div>
@@ -31,15 +47,33 @@ const CharactersList = ({dataChars, maximum}) => {
       <button onClick={addCharacter}>Añadir nuevo</button>
       <button onClick={() => seePosition(maximum)}>Alerta del maximum</button>
 
-      <ul>
-        {
-          dataChars
-            .characters
-            .map((character, index) =>
-              <Character charData={character} key={index} />
-            )
-        }
-      </ul>
+      {/* ------------------------------------------------- */}
+
+      {
+        dataChars.characters.length > 0
+        ?
+          <div>
+            <p>Filtrar por: <input type="text" onChange={handlerSearch} /></p>
+
+            <ul>
+              {
+                dataChars
+                  .characters
+                  .filter(character =>
+                    character.name.length > 0 &&
+                    character.name.startsWith(search)
+                  )
+                  .map((character, index) =>
+                    <Character charData={character} key={index} />
+                  )
+              }
+            </ul>
+
+            <p>Total: {totalSaiyans}</p>
+          </div>
+        :
+          <p>No hay personajes cargados</p>
+      }
     </div>
   )
 }
