4cf473e6c6339f0aa01ab6392c769c10b821f48a
DevIdCarvajal <davidcarvajalg@gmail.com>
2023-05-23 19:13:39 +0200

    localStorage hook example

--- examples/src/components/Asteroids.jsx
+++ examples/src/components/Asteroids.jsx
@@ -3,9 +3,19 @@ import { useDispatch, useSelector } from "react-redux"
 
 import { fetchAsteroids } from '../slices/asteroids/asteroidsSlice'
 
+import useLocalStorage from '../hooks/useLocalStorage'
+
 const Asteroids = () => {
   const dispatch = useDispatch()
   const asteroids = useSelector(state => state.asteroids)
+
+  const [localStorage, setLocalStorage] = useLocalStorage('asteroids', null)
+
+  useEffect(() => {
+    if (!localStorage) {
+      setLocalStorage([])
+    }
+  }, [])
 
   useEffect(() => {
     dispatch(fetchAsteroids())
@@ -13,12 +23,33 @@ const Asteroids = () => {
 
   return (
     <div>
+      <h2>Datos de la API</h2>
+
       <ul>
         {
           asteroids.map((asteroid, index) =>
             <li key={index} style={{
               color: asteroid.warning ? "red": "black"
-            }}>{asteroid.name}</li>
+            }}>
+              {asteroid.name}
+              <button onClick={() => setLocalStorage([...localStorage, asteroid])}>Guardar</button>
+            </li>
+          )
+        }
+      </ul>
+
+      <h2>Datos guardados</h2>
+
+      <ul>
+        {
+          localStorage
+          &&
+          localStorage.map((asteroid, index) =>
+            <li key={index} style={{
+              color: asteroid.warning ? "red": "black"
+            }}>
+              {asteroid.name}
+            </li>
           )
         }
       </ul>
